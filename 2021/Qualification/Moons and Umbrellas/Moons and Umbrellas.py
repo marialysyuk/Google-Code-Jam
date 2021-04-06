413e16fb330d32501c0fca94c85ef910a0dfe3a4
marialysyuk <44041257+marialysyuk@users.noreply.github.com>
2021-04-06 16:26:30 +0300

    Add files via upload

---
+++ 2021/Qualification/Moons and Umbrellas/Moons and Umbrellas.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+
+def moons_umbrellas(CJ_price, JC_price, word):
+    CJ = 0
+    JC = 0
+    C = 0
+    J = 0
+    for i in range(len(word)):
+        if word[i] == 'C':
+            if C == 0:
+                C += 1
+                if J == 1:
+                    JC += 1
+                    J = 0
+        if word[i] == 'J':
+            if J == 0:
+                J += 1
+                if C == 1:
+                    CJ += 1
+                    C = 0
+    result = CJ*CJ_price+JC*JC_price
+    return result
+    
+nums = sys.stdin.readlines()
+content = [x.split() for x in nums]
+content = content[1:]
+
+cases = 0
+i = 0 
+while i < len(content):
+    cases += 1
+    CJ_price = int(content[i][0])
+    JC_price = int(content[i][1])
+    word = content[i][2]
+    print("Case #"+str(cases)+": "+str(moons_umbrellas(CJ_price, JC_price, word)))
+    i += 1
+
